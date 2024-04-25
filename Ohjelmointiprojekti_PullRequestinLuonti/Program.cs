5779683c5c041365a9e17990a6aa3528880e6e66
Lenovo <Lenovo@LAPTOP-E211JOL9>
2024-04-25 12:49:02 +0300

    Add project files.

---
+++ Ohjelmointiprojekti_PullRequestinLuonti/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ohjelmointiprojekti_PullRequestinLuonti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
